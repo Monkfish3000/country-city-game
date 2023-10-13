e0359bb2b6d72695c8f749bfa7db0f3aa9bfe1c6
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-10-13 14:40:26 +0200

    button bg blue on click

--- src/game/CountryCapitalGame.tsx
+++ src/game/CountryCapitalGame.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import { useState } from 'react';
+
+type ButtonState = 'SELECTED' | 'WRONG' | 'DEFAULT';
+
+type Option = {
+  value: string;
+  state: ButtonState;
+};
 
 const CountryCapitalGame = ({ data }: { data: Record<string, string> }) => {
   const countries = Object.keys(data);
   const capitals = Object.values(data);
 
-  const options = [...countries, ...capitals];
+  const [options, setOptions] = useState<Option[]>(
+    [...countries, ...capitals].map((value) => ({
+      value,
+      state: 'DEFAULT',
+    }))
+  );
 
   return (
     <>
       {options.map((option) => (
-        <button>{option}</button>
+        <button
+          className={`${option.state === 'SELECTED' ? 'selected' : ''}`}
+          onClick={() => {
+            setOptions(
+              options.map((opt) => {
+                return opt === option
+                  ? {
+                      ...opt,
+                      state: 'SELECTED',
+                    }
+                  : opt;
+              })
+            );
+          }}
+          key={option.value}
+        >
+          {option.value}
+        </button>
       ))}
     </>
   );
